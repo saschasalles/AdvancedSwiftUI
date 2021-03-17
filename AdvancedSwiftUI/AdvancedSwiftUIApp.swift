3b9fb32856f2558c0744bca257ff99a409bf33f3
Sascha Sallès <sascha.salles@ynov.com>
2021-03-17 10:26:19 +0100

    Initial Commit

---
+++ AdvancedSwiftUI/AdvancedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedSwiftUIApp.swift
+//  AdvancedSwiftUI
+//
+//  Created by Sascha Sallès on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdvancedSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdvancedSwiftUI
+//
+//  Created by Sascha Sallès on 17/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
